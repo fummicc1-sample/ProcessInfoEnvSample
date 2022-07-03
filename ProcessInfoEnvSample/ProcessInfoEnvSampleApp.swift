4464b33d91b9b0f4ba11fe5c5c47e61ebe4c7e0b
fummicc1 <fumiya.tennis1@gmail.com>
2022-07-04 00:45:30 +0900

    Initial Commit

---
+++ ProcessInfoEnvSample/ProcessInfoEnvSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcessInfoEnvSampleApp.swift
+//  ProcessInfoEnvSample
+//
+//  Created by Fumiya Tanaka on 2022/07/04.
+//
+
+import SwiftUI
+
+@main
+struct ProcessInfoEnvSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
